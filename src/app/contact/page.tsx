85f5ff068368c41265eca32de2a9d12e0208e938
fl-yashrajsamani <yashrajlearntech@gmail.com>
2025-04-02 12:31:50 +0530

    5th error

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -8,7 +8,6 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Mail, Phone, MapPin, Send } from "lucide-react";
-// import { useSearchParams } from "next/navigation";
 import Link from "next/link";
 
 export default function ContactPage() {
